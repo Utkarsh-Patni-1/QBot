82cd9e5012f80ef4525bee135da57e7cb71b7edd
Utkarsh Patni <v-upatni@microsoft.com>
2022-03-17 15:07:43 +0530

    edu updated env.ts

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbot-utkarsh-edu.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbot-utkarsh-edu-questions.azurewebsites.net",
+    apiBaseUrl: "https://qbot-tcs-edu-test.azurewebsites.net/api/Request/",
+    selfUrl: "https://qbot-tcs-edu-test-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "569188aa-58a1-40d3-bb39-5a0c569ea981",
-        clientId: "c5acddb9-aa14-4055-8860-3962be8c88b1",
+        clientId: "dfffba11-81e2-458d-9763-a598ae95aeb4",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
